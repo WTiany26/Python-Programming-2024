a6cd33d02860e0893045de6f1b47ada4388247a9
Willys Tiany <t.willys26@gmail.com>
2024-08-08 18:54:39 +0300

    Another way of handling errors using the same Try and Except clauses.

--- zeroDivide.py
+++ zeroDivide.py
@@ -1,10 +1,10 @@
 def spam(divideBy):
-    try:
-        return 42 / divideBy
-    except ZeroDivisionError:
-        print('Error: Invalid argument')
+    return 42 / divideBy
 
-print(spam(2))
-print(spam(12))
-print(spam(0))
-print(spam(1))
+try:
+    print(spam(2))
+    print(spam(12))
+    print(spam(0))
+    print(spam(1))
+except ZeroDivisionError:
+    print('Error: Invalid argument.')
